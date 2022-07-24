39b297b98f0426eeddbf7355e0a71073d183ee45
AlbertHW123 <108035115+AlbertHW123@users.noreply.github.com>
2022-07-23 22:10:18 -0700

    Add files via upload

---
+++ data/sources 3.R
@@ -0,0 +1,17 @@
+data <- read.csv("Ski_resort.csv")
+
+#1. I downloaded it from 
+#https://www.kaggle.com/datasets/agustinpugliese/ski-resort-data?resource=download
+
+#2. The data was collected by a Argentinian student who currently lived in
+# Buenos Aries with investigation on data clustering on when/how/where did people
+# buy tickets for skating, what prices they boughts tickets for
+# and also reflecting their schedules for skating during
+# that period.
+
+#3. There are 907 observations in the data.
+
+#4. There are 12 features (columns) in the data.
+
+#5. By using strategic r codes with particular syntax, we can ultimately solve all 
+# the questions which are being mentioned above.
