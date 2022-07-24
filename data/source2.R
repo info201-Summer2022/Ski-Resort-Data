adf85122df5206a13df1c4efbdb2d059f9b95496
LetitiaW13 <ws010901@uw.edu>
2022-07-23 21:21:58 -0700

    Update source2

--- data/source2.R
+++ data/source2.R
@@ -1,1 +1,16 @@
-#
+# Where did you download the data (e.g., a web URL)?
+# https://www.statista.com/statistics/983532/ski-gear-and-equipment-market-value-worldwide/
+
+# How was the data collected or generated? Make sure to explain who collected the data (not necessarily the same people that host the data), and who or what the data is about?
+# MarketWatch and Statista collected this data.
+# They collected this data worldwide and based on current data to forecast it up to 2028.
+# The data is about global ski gear and equipment market size.
+
+# How many observations (rows) are in your data?
+# There're 11 rows in my data.
+
+# How many features (columns) are in the data?
+# There're two columns in my data.
+
+# What questions (from above) can be answered using the data in this dataset? 
+# What is the ski gear & equipment changes from year to year.
