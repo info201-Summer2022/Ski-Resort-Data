d58fdf3b98a9828d7fcf83261ef96e6d1e09f5a7
ShiYiShe <yshi6@uw.edu>
2022-07-24 18:37:34 -0700

    source 1

---
+++ data/sources 1.R
@@ -0,0 +1,16 @@
+# Where did you download the data (e.g., a web URL)?
+# https://www.kaggle.com/datasets/thomasnibb/european-ski-resorts
+
+# How was the data collected or generated? Make sure to explain who collected the data
+# (not necessarily the same people that host the data), and who or what the data is about?
+# Svitlana Bozhenko, Leo Auri, and Shuai Liu are top contributers.
+# They collected from the ski resort data has been obtained from Ski-resort-stats.com.
+
+# How many observations (rows) are in your data?
+# There're 17 rows in my data.
+
+# How many features (columns) are in the data?
+# There're 376 rows in my data.
+
+# What questions (from above) can be answered using the data in this dataset? 
+# What is the highest point of each resorts in European?
