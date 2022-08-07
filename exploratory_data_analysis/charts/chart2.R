b16183523df7d6cd8a6e81e207efce48ee5c8169
ShiYiShe <108631128+ShiYiShe@users.noreply.github.com>
2022-08-06 23:18:31 -0700

    Update chart2.R

--- exploratory_data_analysis/charts/chart2.R
+++ exploratory_data_analysis/charts/chart2.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 data_df <- read.csv("Ski_resort.csv")
 num_row <- nrow(data_df)
 
@@ -8,4 +10,4 @@ num_daily <- nrow(filter(type_df, Type == "Daily"))
 num_red <- nrow(filter(type_df, Type == "Red HEROIC"))
 
 data_plot <- c(num_season, num_daily, num_red)
-barplot(data_plot, main = "Number of Type", xlab = "Types", names.arg = c("Season", "Daily", "Red HEROIC"))
+barplot(data_plot, main = "Number of Each Type", xlab = "Types", ylab = "Number", names.arg = c("Season", "Daily", "Red HEROIC"))
