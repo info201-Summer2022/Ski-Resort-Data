882a0f540702ae5910a4fff1341ca12e8d2bb450
ShiYiShe <108631128+ShiYiShe@users.noreply.github.com>
2022-08-06 17:29:07 -0700

    Create chart2.r

---
+++ exploratory_data_analysis/charts/chart2.r
@@ -0,0 +1,11 @@
+data_df <- read.csv("Ski_resort.csv")
+num_row <- nrow(data_df)
+
+type_df <- select(data_df, "Type")
+different_type <- unique(type_df)
+num_season <- nrow(filter(type_df, Type == "Season"))
+num_daily <- nrow(filter(type_df, Type == "Daily"))
+num_red <- nrow(filter(type_df, Type == "Red HEROIC"))
+
+data_plot <- c(num_season, num_daily, num_red)
+barplot(data_plot, main = "Number of Type", xlab = "Types", names.arg = c("Season", "Daily", "Red HEROIC"))
