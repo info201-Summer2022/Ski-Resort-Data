84911ac7709bfb0ddc6d02a75ae1d72ab8c107c1
AlbertHW123 <108035115+AlbertHW123@users.noreply.github.com>
2022-08-07 17:41:42 -0700

    Update chart3.R

--- exploratory_data_analysis/charts/chart3.R
+++ exploratory_data_analysis/charts/chart3.R
@@ -7,3 +7,7 @@ ggplot(bought_in_df, aes(x=Selling_price, y=Total_price)) +
   geom_point(colour = 4) + 
   labs(title = "Comparison between Selling Price and Total Price")
 
+# I include this chart to show how would the selling price compare with total price so that the audiences would have an understanding on the relationship
+# between what strategies the sellers would want to apply when they boost up the ticket price in total. 
+# It reveals the fact that the sellers would not give up interests they could get for an exchange of higher skier population buying tickets, which means
+# it is hard to see huge bump-ups for a particular strategy of tickets when the total ticket prices go up or go down.
